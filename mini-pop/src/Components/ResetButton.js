95421f95fa1c91d640ebbda1185a5110d497168a
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-11-23 14:05:05 -0500

    Reset Button

--- mini-pop/src/Components/ResetButton.js
+++ mini-pop/src/Components/ResetButton.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ResetButton = (props) => {
-  return <div></div>;
+  return (
+    <div className="resetButton" onClick={props.onReset}>
+      <span>
+        <FontAwesomeIcon icon="recycle" />
+      </span>
+    </div>
+  );
 };
 
 export default ResetButton;
