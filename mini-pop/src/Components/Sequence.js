c7684657a10ddab071da47d161723f774f132f15
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-11-23 14:35:02 -0500

    constructing sequencer

--- mini-pop/src/Components/Sequence.js
+++ mini-pop/src/Components/Sequence.js
@@ -1,7 +1,27 @@
 import React from "react";
+import _ from "lodash";
 
 const Sequence = (props) => {
-  return <div></div>;
+  return (
+    <div className="sequenceBox">
+      {_.map(props.checked[props.row], (isBoxChecked, i) => (
+        <div
+          onClick={() => {
+            props.onToggle(i, props.row);
+          }}
+          className={_.chain([
+            isBoxChecked,
+            props.isActive[props.row][i] && !isBoxChecked,
+            props.isActive[props.row][i] && isBoxChecked,
+          ])
+            .compact()
+            .join("")
+            .value()}
+          key={i}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Sequence;
