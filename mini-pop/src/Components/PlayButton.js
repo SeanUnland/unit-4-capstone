7ca58f52924c78708c98a0d577bd55181a5e9c1d
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-11-23 14:02:10 -0500

    Time Signature component

--- mini-pop/src/Components/PlayButton.js
+++ mini-pop/src/Components/PlayButton.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PlayButton = (props) => {
-  return <div></div>;
+  return (
+    <div className="playButton" onClick={props.onTogglePlay}>
+      <span>
+        {props.isPlaying ? (
+          <FontAwesomeIcon icon="stop" />
+        ) : (
+          <FontAwesomeIcon icon="play" />
+        )}
+      </span>
+    </div>
+  );
 };
 
 export default PlayButton;
--- mini-pop/src/Components/TimeSignature.js
+++ mini-pop/src/Components/TimeSignature.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const TimeSignature = (props) => {
-  return <div></div>;
+  return (
+    <div className="timeSignature">
+      <select
+        name="length"
+        id="row-length"
+        value={props.sequenceLength}
+        onChange={(e) => {
+          props.onLengthChange(e.target.value);
+        }}
+      >
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+      </select>
+    </div>
+  );
 };
 
 export default TimeSignature;
